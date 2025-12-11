adf129fa20f66ba638d3667aad4d41253ea3fbfb
Shivamtiwari2506 <shivamtiwari0784@gmail.com>
2025-12-11 13:47:39 +0530

    fixed cors issue again

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ const PORT = process.env.PORT || 5000;
 connectDB();
 
 app.use(cors({
-  origin: `http://localhost:5173` || `${process.env.CLIENT_URL}`,
+  origin: [process.env.CLIENT_URL, "http://localhost:5173"].filter(Boolean),
   credentials: true,
 }
 ));
